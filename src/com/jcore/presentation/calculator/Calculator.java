824ad3863eab6712421b25e6140519225ae1633e
Jacob van Lingen <jacobvanlingen@hotmail.com>
2021-03-26 15:53:25 +0100

    Complete basic calculator

--- src/com/jcore/presentation/calculator/Calculator.java
+++ src/com/jcore/presentation/calculator/Calculator.java
@@ -3,9 +3,12 @@ package com.jcore.presentation.calculator;
 import static com.jcore.lib.model.List.list;
 import static com.jcore.lib.model.TailCall.ret;
 import static com.jcore.lib.model.TailCall.sus;
+import static com.jcore.presentation.calculator.Operation.DIVIDE;
+import static com.jcore.presentation.calculator.Operation.MINUS;
+import static com.jcore.presentation.calculator.Operation.MULTIPLY;
+import static com.jcore.presentation.calculator.Operation.PLUS;
 import static com.jcore.presentation.calculator.Operation.getBySymbol;
 
-import java.io.IOException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -30,40 +33,54 @@ public class Calculator {
 			}
 
 			if (!IS_VALID.matcher(input).matches()) {
-				System.out.println("Cannot compute current input");
+				System.out.println("Parse error");
 				continue;
 			}
 
-			final Tuple<List<String>, List<Operation>> partsAndOperators = foldRightWitPrev(fromString(input), new Tuple<>(list(), list()), x -> y -> z -> getBySymbol(x).exists()
-					? new Tuple<>(z._1, z._2.cons(getBySymbol(x).getOrElse(() -> null)))
-					: new Tuple<>(!y.exists() || y.exists(k -> getBySymbol(k).exists())
-						? z._1.cons(String.valueOf(x))
-						: z._1.setHead(x + z._1.head()),
-					z._2));
+			final var partsAndOperators = processInput(input);
+			final var r = calculate(partsAndOperators, o -> o == MULTIPLY || o == DIVIDE);
+			final var k = calculate(r, o -> o == PLUS || o == MINUS);
 
-			// Do actual calculation
-
-			System.out.println(partsAndOperators);
+			System.out.println(k._1.head());
 		}
 	}
 
-	private static List<Character> fromString(final String s) {
-		// TODO make immutable
-		List<Character> list = list();
-		for (char c : s.toCharArray()) {
-			list = list.cons(c);
-		}
+	private static Tuple<List<Double>, List<Operation>> processInput(final String input) {
+		final Tuple<List<String>, List<Operation>> a = foldRightWitPrev(fromString(input, list()), new Tuple<>(list(), list()), x -> y -> z -> getBySymbol(x).exists()
+				? new Tuple<>(z._1, z._2.cons(getBySymbol(x).getOrElse(() -> null)))
+				: new Tuple<>(!y.exists() || y.exists(k -> getBySymbol(k).exists())
+				? z._1.cons(String.valueOf(x))
+				: z._1.setHead(x + z._1.head()),
+				z._2));
 
-		return list.reverse();
+		return new Tuple<>(a._1.map(Double::valueOf), a._2);
+	}
+
+	private static List<Character> fromString(String s, List<Character> acc) {
+		return s.isEmpty()
+				? acc
+				: fromString(s.substring(1), acc.cons(s.charAt(0)));
+	}
+
+	private static Tuple<List<Double>, List<Operation>> calculate(Tuple<List<Double>, List<Operation>> toCalculate, ƒ<Operation, Boolean> f) {
+		return _calculateMultiplyDivide(toCalculate._1, toCalculate._2, new Tuple<>(list(), list()), f).eval();
+	}
+
+	private static TailCall<Tuple<List<Double>, List<Operation>>> _calculateMultiplyDivide(List<Double> numbers, List<Operation> operators, Tuple<List<Double>, List<Operation>> acc, ƒ<Operation, Boolean> f) {
+		return operators.isEmpty()
+				? ret(new Tuple<>(acc._1.cons(numbers.head()), acc._2))
+				: sus(() -> f.apply(operators.head())
+					? _calculateMultiplyDivide(numbers.tail().tail().cons(operators.head().apply(numbers.head(), numbers.tail().head())), operators.tail(), acc, f)
+					: _calculateMultiplyDivide(numbers.tail(), operators.tail(), new Tuple<>(acc._1.cons(numbers.head()), acc._2.cons(operators.head())), f));
 	}
 
 	private static <E, B> B foldRightWitPrev(List<E> list, B identity, ƒ<E, ƒ<Ø<E>, ƒ<B, B>>> f) {
-		return foldRightWithPrev_(list.reverse(), identity, Ø.none(), f).eval();
+		return _foldRightWithPrev(list.reverse(), identity, Ø.none(), f).eval();
 	}
 
-	private static <E, B> TailCall<B> foldRightWithPrev_(List<E> list, B acc, Ø<E> prev, ƒ<E, ƒ<Ø<E>, ƒ<B, B>>> f) {
+	private static <E, B> TailCall<B> _foldRightWithPrev(List<E> list, B acc, Ø<E> prev, ƒ<E, ƒ<Ø<E>, ƒ<B, B>>> f) {
 		return list.isEmpty()
 				? ret(acc)
-				: sus(() -> foldRightWithPrev_(list.tail(), f.apply(list.head()).apply(prev).apply(acc), Ø.some(list.head()), f));
+				: sus(() -> _foldRightWithPrev(list.tail(), f.apply(list.head()).apply(prev).apply(acc), Ø.some(list.head()), f));
 	}
 }
